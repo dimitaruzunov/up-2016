8d1545638f6c8f03bc2a0fb56880b33272fdc40e
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-10-17 00:25:57 +0300

    Change quadratic-equation program to check for cases when the equation is not quadratic

--- ex01/quadratic-equation.cpp
+++ ex01/quadratic-equation.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 #include <cmath>
 
+using std::cin;
+using std::cout;
+
 /* Solves quadratic equation ax^2 + bx + c = 0 over the real numbers */
 int main() {
   double a = 0;
-  std::cout << "a = ";
-  std::cin >> a;
+  cout << "a = ";
+  cin >> a;
 
   double b = 0;
-  std::cout << "b = ";
-  std::cin >> b;
+  cout << "b = ";
+  cin >> b;
 
   double c = 0;
-  std::cout << "c = ";
-  std::cin >> c;
+  cout << "c = ";
+  cin >> c;
 
-  double discriminant = b * b - 4 * a * c;
-
-  if (discriminant < 0) {
-    std::cout << "No real roots.\n";
-  } else if (discriminant == 0) {
-    std::cout << "x1 = x2 = " << -b / (2 * a) << '\n';
+  if (a == 0) {
+    if (b == 0) {
+      if (c == 0) {
+        cout << "Every real x is a solution.\n";
+      } else {
+        cout << "No real roots.\n";
+      }
+    } else {
+      cout << "x = " << -c / b << '\n';
+    }
   } else {
-    std::cout << "x1 = " << (-b - sqrt(discriminant)) / (2 * a) << '\n';
-    std::cout << "x2 = " << (-b + sqrt(discriminant)) / (2 * a) << '\n';
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+      cout << "No real roots.\n";
+    } else if (discriminant == 0) {
+      cout << "x1 = x2 = " << -b / (2 * a) << '\n';
+    } else {
+      cout << "x1 = " << (-b - sqrt(discriminant)) / (2 * a) << '\n';
+      cout << "x2 = " << (-b + sqrt(discriminant)) / (2 * a) << '\n';
+    }
   }
 
   return 0;
